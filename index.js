ae476ac92901ddfabb7a7d80c62451916242e1dd
Mikeal Rogers <mikeal.rogers@gmail.com>
2018-11-09 19:00:13 -0800

    fix: clearing repo, pushed too early.

--- index.js
+++
@@ -1,7 +0,0 @@
-let { runner } = require('./lib/util')
-
-let createData = require('./data/shallow-100n-tiny')
-let createNetwork = require('./networks/1-peer-no-throttle')
-let replicator = require('./replicators/bitswap-serial')
-
-runner(createData, createNetwork, replicator).then(r => console.log(r))
--- networks/1-peer-no-throttle.js
+++
@@ -1,6 +0,0 @@
-const {createClientServer} = require('../lib/util')
-
-module.exports = async store => {
-  let [server, client] = await createClientServer()
-  return [[server, client]]
-}
